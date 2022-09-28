9145e973a40f6e89e8e2848bae0181eeaf20b0ef
Sarath-KrishnanBiz <sarath.krishnan@bizcloudexperts.com>
2022-09-29 01:46:25 +0530

    commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import Navigation from './Navigation';
+import Test from './Admindashboard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Test/>
+  </React.StrictMode>
+);
+
